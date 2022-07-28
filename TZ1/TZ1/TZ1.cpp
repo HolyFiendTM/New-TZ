a37faa6e4d91ed9af7e25fce540de166bcfbb898
HolyFiendTM <109869937+HolyFiendTM@users.noreply.github.com>
2022-07-28 17:01:57 +0300

    Add files via upload

---
+++ TZ1/TZ1/TZ1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+    ifstream f;
+    f.open("CreativeTask1.txt");
+    int max_count = 0;
+    int count = 0;
+    string s;
+    getline(f, s);
+    f.close();
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '-') {
+            count++;
+            max_count = max(max_count, count);
+        }
+        else {
+            count = 0;
+        }
+    }
+    cout << max_count;
+}
+//объяснение:
+//проходимся по строке; 
+//если символ — "-", тогда count увеличиваем на 1 и сразу же сравниваем с текущим max_count, заносим туда наибольшее значение;
+//если символ — "+", тогда count приравниваем к нулю;
+//выводим max_count - ответ
---
+++ TZ2.1/TZ2/TZ2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+    ifstream f;
+    f.open("CreativeTask2.txt");
+    int max_count = 0;
+    int count = 0;
+    string s;
+    getline(f, s);
+    f.close();
+    for (int i = 65; i <= 90; i++) {
+        int flag = 0;
+        for (int j = 0; j < s.size(); j++) {
+            if (s[j] == char(i)) {
+                flag = 1;
+                count++;
+            }
+        }
+        max_count = max(max_count, count);
+        count = 0;
+        if (flag == 1) {
+            cout << char(i);
+        }
+    }
+    cout << max_count;
+}
+//объяснение:
+//по порядку проходимся по всем прописным буквам латинского алфавита (будем называть рассматриваемую букву на данной итерации текущей);
+//проходимся по строке;
+//если символ — текущая буква, то отмечаем, что нашли её (flag = 1), и увеличиваем count на 1 (показывает, сколько раз нашли текущую букву);
+//сравниваем count и max_count, во вторую переменную заносим наибольшее значение;
+//если мы нашли текущую букву в строке (flag == 1), то выводим её;
+//выводим max_count — количество раз, сколько встретилась самая часто употребляемая буква
+
